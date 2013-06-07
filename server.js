096dea72394cb55e903fe783e0406274e5139d1a
Garrett Everding <garrett.everding92@gmail.com>
2013-06-07 08:00:09 -0400

    checkout repo on trigger works

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ var express = require('express')
   , redis = require('redis')
   , mongose = require('mongoose')
   , routes = require('./lib/routes')
+  , path = require('path')
   , kue = require('kue');
+
+var settings = {
+  git_path: "/home/geverding/dev/"
+}
+
+app.settings = settings
 
 kue.redis.createClient = function() {
   var client = redis.createClient(6378, "127.0.0.1");
@@ -15,18 +22,15 @@ kue.redis.createClient = function() {
 var jobs = kue.createQueue();
 
 app.configure(function() {
+  app.use(express.bodyParser());
   app.use(app.router);
   app.use(express.logger('dev'));
   app.use(express.basicAuth('foo', 'bar'));
   app.use(kue.app);
 });
 
-app.get('/', function(req, res) {
-  console.log(req);
-  res.redirect('active');
-});
 
-routes.init(app);
+routes.init(app, jobs);
 
 app.listen(5050);
 console.log("Express server listening on port %d in %s mode", 5050, app.settings.env);
