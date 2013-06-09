c3377e4ecedf59092a9728574de182a6ecc35d09
Garrett Everding <garrett.everding92@gmail.com>
2013-06-09 11:23:23 -0400

    pulling kue cause its shit

--- server.js
+++ server.js
@@ -8,13 +8,11 @@ var express = require('express')
   , http = require('http')
   , mongoose = require('mongoose')
   , path = require('path')
-  , kue = require('kue')
   , redis = require('redis')
-  , workers = require('./jobs')
   , yaml = require('libyaml')
-  , lessMiddleware = require('less-middleware');
 
 var app = express();
+
 yaml.readFile('config.yaml', function(err, doc){
   if(err){
     console.error('Could not read config')
@@ -45,15 +43,13 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(kue.app)
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-routes.init(app,jobs)
-workers.init(jobs)
+routes.init(app)
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
